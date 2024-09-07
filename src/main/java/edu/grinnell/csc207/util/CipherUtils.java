5b6047182369721c449f3de852cf4212fd9d76b4
khanhdo05 <khanhphuongdo28@gmail.com>
2024-09-07 13:00:17 -0500

    Closes #6

--- src/main/java/edu/grinnell/csc207/util/CipherUtils.java
+++ src/main/java/edu/grinnell/csc207/util/CipherUtils.java
@@ -85,6 +85,17 @@ public class CipherUtils {
     // avoid division by zero
     if (key.length() == 0) {
     return str;
+    } // if key is empty, return original string
+
+    char[] input = str.toCharArray();
+    char[] keyChars = key.toCharArray();
+
+    for (int i = 0; i < input.length; i++) {
+      input[i] = CipherUtils.int2letter(
+          CipherUtils.letter2int(input[i]) + CipherUtils.letter2int(keyChars[i % keyChars.length]));
+    } // for loop that encrypt each character
+
+    return new String(input);
   } // vigenereEncrypt(String, String)
 
   /**
